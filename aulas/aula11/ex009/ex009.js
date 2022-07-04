f48121e579ab988d3255424e6bb427db2aaf191e
João Antonio <jao.ratones@gmail.com>
2022-07-04 16:31:08 -0300

    040722 2

--- aulas/aula11/ex009/ex009.js
+++ aulas/aula11/ex009/ex009.js
@@ -1,11 +1,12 @@
 const verificar = document.querySelector('#verificar')
 verificar.addEventListener('click', clicou)
-function clicou() {
-    const nacionalidade = document.querySelector('#nacionalidade')
-    const resposta = document.querySelector('#resposta')
 
-    if (nacionalidade == 'Brasil') {
-        resposta.innerHTML = `Você é <strong>Brasileiro</strong>`
+function clicou() {
+    const nacionalidade = document.querySelector('#nacionalidade').value.toLowerCase()
+    const resposta = document.querySelector('.resposta')
+    
+    if (nacionalidade == 'brasil') {
+        resposta.innerHTML = `Você é <strong>Brasileiro</strong>!`
     } else {
         resposta.innerHTML = `Você é <strong>Estrangeiro</strong>!`
     }
